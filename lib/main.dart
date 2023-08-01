5abd6b86a71a2a5cac5c27e24a523bee3e728ecd
tradelab <jaswanth@tradelab.in>
2023-08-01 09:06:40 +0530

    rotating container on y axis 360 degrees.

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin{
+
+  late AnimationController _controller;
+  late Animation  _animation;
+
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _controller = AnimationController(vsync: this,duration: const Duration(seconds: 2));
+    _animation = Tween(begin: 0,end: 2 * pi).animate(_controller);
+    _controller.repeat();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: AnimatedBuilder(
+            animation: _animation,
+            builder: (context, child) {
+            return Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.identity()..rotateX(_animation.value),
+              child: Container(
+                height: 150,
+                width: 150,
+                color: Colors.blue,
+              ),
+            );
+          }) ,
+
+        ) ,
+    );// This trailing comma makes auto-formatting nicer for build methods.
+
+  }
+}
